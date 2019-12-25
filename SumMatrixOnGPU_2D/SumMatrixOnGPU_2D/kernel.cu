bca19027d279dff766743f229797688aec16e0c2
NeilWangziyu <535220146@qq.com>
2019-12-25 10:42:59 +0800

    Merry Xmas

--- SumMatrixOnGPU_2D/SumMatrixOnGPU_2D/kernel.cu
+++ SumMatrixOnGPU_2D/SumMatrixOnGPU_2D/kernel.cu
@@ -1,10 +1,28 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <cuda_runtime.h>
 
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <stdio.h>
+#include <iostream>
+#include <Windows.h>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+#define CHECK(call)\
+{				\
+	const cidaError_t error = call;	\
+	if (error != cudaSuccess)\
+	{\
+		printf("Erroe: %s: %d,  ", __FILE__, __LINE__);\
+		printf("code: %d, reason: %s\n, ", error, cudaGetErrorString);\
+		exit(1);\
+	}\
+}\
+
 
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
@@ -42,6 +60,70 @@ __global__ void SumMatrixOnGPU2D(float *MatA, float *MatB, float *MatC, int nx, 
 	}
 	
 }
+void initialInx(int *ip, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		ip[i] = i;
+	}
+}
+
+void printmatrix(int *C, const int nx, const int ny)
+{
+	int *ic = C;
+	printf("Matrix: (%d.%d)\n", nx, ny);
+	for (int iy = 0; iy < ny; iy++)
+	{
+		for (int ix = 0; ix < nx; ix++)
+		{
+			printf("%3d", ic[ix]);
+		}
+		ic += nx;
+		printf("\n");
+	}
+}
+
+void printmatrix(float *C, const int nx, const int ny)
+{
+	float *ic = C;
+	printf("Matrix: (%d.%d)\n", nx, ny);
+	for (int iy = 0; iy < ny; iy++)
+	{
+		for (int ix = 0; ix < nx; ix++)
+		{
+			printf("%3f\t", ic[ix]);
+		}
+		ic += nx;
+		printf("\n");
+	}
+}
+
+void initialData(float *ip, int size)
+{
+	time_t t;
+	srand((unsigned int)time(&t));
+	for (int i = 0; i < size; i++)
+	{
+		ip[i] = (float)(rand() & 0xFF) / 10.0f;
+	}
+}
+
+void CheckResult(float *hostref, float *gpuRef, const int N)
+{
+	double epsilon = 1.0E-8;
+	bool match = 1;
+	for (int i = 0; i < N; i++)
+	{
+		if (abs(hostref[i] - gpuRef[i]) > epsilon)
+		{
+			match = 0;
+			printf("Array do not match!\n");
+			printf("host %5.2f gpu %5.2f at current %d\n", hostref[i], gpuRef[i], i);
+			break;
+		}
+	}
+	if (match) printf("Array match.\n\n");
+}
 
 int main()
 {
@@ -67,6 +149,83 @@ int main()
         fprintf(stderr, "cudaDeviceReset failed!");
         return 1;
     }
+
+	printf("Finished Default \n ----------------------- \n");
+
+	int dev = 0;
+	cudaDeviceProp deviceProb;
+	//CHECK(cudaGetDeviceProperties(&deviceProb, dev));
+	//printf("Using Device: %d: %s \n", dev, deviceProb.name);
+	printf("Using Device: %d\n", dev);
+
+	int nx = 1<<3;
+	int ny = 1<<3;
+	int nxy = nx * ny;
+	int nBytes = nxy * sizeof(float);
+	printf("matrix size: nx: %d, ny: %d", nx, ny);
+
+
+	//malloc
+	float *h_A, *h_B, *hostRef, *gpuRef;
+	h_A = (float *)malloc(nBytes);
+	h_B = (float *)malloc(nBytes);
+	hostRef = (float *)malloc(nBytes);
+	gpuRef = (float *)malloc(nBytes);
+	
+
+	//init
+	initialData(h_A, nxy);
+	initialData(h_B, nxy);
+
+	printmatrix(h_A, nx, ny);
+	printmatrix(h_B, nx, ny);
+
+
+	memset(hostRef, 0, nBytes);
+	memset(gpuRef, 0, nBytes);
+
+	sumMatrixOnHost(h_A, h_B, hostRef, nx, ny);
+
+	float *d_MatA, *d_MatB, *d_MatC;
+	cudaMalloc((void**)&d_MatA, nBytes);
+	cudaMalloc((void**)&d_MatB, nBytes);
+	cudaMalloc((void**)&d_MatC, nBytes);
+
+
+	cudaMemcpy(d_MatA, h_A, nBytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_MatB, h_B, nBytes, cudaMemcpyHostToDevice);
+
+	int dimx = 32;
+	int dimy = 32;
+	dim3 block(dimx, dimy);
+	dim3 grid((nx + block.x - 1) / block.x, (ny + block.y - 1) / block.y);
+
+	SumMatrixOnGPU2D <<<grid, block >>> (d_MatA, d_MatB, d_MatC, nx, ny);
+
+
+
+	cudaMemcpy(gpuRef, d_MatC, nBytes, cudaMemcpyDeviceToHost);
+
+	printf("host result:\n");
+	printmatrix(hostRef, nx, ny);
+
+	printf("gpu result:\n");
+
+	printmatrix(gpuRef, nx, ny);
+
+	CheckResult(hostRef, gpuRef, nxy);
+
+
+	cudaFree(d_MatA);
+	cudaFree(d_MatB);
+	cudaFree(d_MatC);
+
+
+
+	free(h_A);
+	free(h_B);
+	free(hostRef);
+	free(gpuRef);
 
     return 0;
 }
